06342d9ed2befce277043dc72454ca076bf29577
Murilo Molina Barone <murilo.m.barone@gmail.com>
2024-08-21 18:10:59 -0300

    primeira implementação do segundo carrossel

--- js/carrossel.js
+++ js/carrossel.js
@@ -13,3 +13,29 @@ const swiper = new Swiper('.swiper', {
         disableOnInteraction: false,
     },
 });
+
+const swiper_card = new Swiper('.swiper-card', {
+    slidesPerView: 3, // Mostra 3 slides por vez
+    spaceBetween: 30, // Espaçamento entre os slides (pode ajustar conforme necessário)
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    loop: true, // Permite rolar infinitamente
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+      },
+      576: {
+        slidesPerView: 2, // Exibe 2 slides em telas menores
+      },
+      320: {
+        slidesPerView: 1, // Exibe 1 slide em telas muito pequenas
+      },
+    },
+  });
+  
