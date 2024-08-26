b3f9fbdf2f584ea29612141da6c520924c50f6b8
Murilo Molina Barone <murilo.m.barone@gmail.com>
2024-08-26 18:03:46 -0300

    primeira implementação (teste) de blocos e modal da seção de rabalhos/serviços

---
+++ js/scriptModal.js
@@ -0,0 +1,39 @@
+var swiper;
+
+function openModal(images) {
+    var swiperWrapper = document.getElementById('swiperWrapper');
+    swiperWrapper.innerHTML = ''; // Limpa slides existentes
+
+    // Adiciona slides ao swiper
+    images.forEach(function (image) {
+        var slide = document.createElement('div');
+        slide.className = 'swiper-slide';
+        slide.style.backgroundImage = `url('${image}')`;
+        swiperWrapper.appendChild(slide);
+    });
+
+    // Inicializa ou atualiza o Swiper
+    if (swiper) {
+        swiper.update();
+    } else {
+        const swiper = new Swiper('.swiper', {
+            loop: true, // Ativa o loop
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            autoplay: {
+                delay: 5000, // Define o tempo entre slides
+                disableOnInteraction: false,
+            },
+        });
+    }
+
+    // Abre o modal
+    var modal = new bootstrap.Modal(document.getElementById('carouselModal'));
+    modal.show();
+}
